56686b6b6e036122b9dff4bb2fda1a75c39d7b3f
Subodh106 <subodhloves106@gmail.com>
2026-07-06 23:17:36 +0545

    Postgres database is configured with docker

--- Backend/src/main/java/com/backend/demo/DemoApplication.java
+++ Backend/src/main/java/com/backend/demo/DemoApplication.java
@@ -2,8 +2,11 @@ package com.backend.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
