c2d6d03c9e77c41c9a05be57e56bba80c3bb55fb
Subodh106 <subodhloves106@gmail.com>
2026-07-07 21:55:38 +0545

    small refactor

--- Backend/src/main/java/com/backend/demo/Config/SecurityConfig.java
+++ Backend/src/main/java/com/backend/demo/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.backend.demo.Config;
 import com.backend.demo.Security.JwtAuthFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @AllArgsConstructor
+@Configuration
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
