2848bd541ebd4c72761472ad4fd08188b47bed1c
Subodh106 <subodhloves106@gmail.com>
2026-07-07 21:53:05 +0545

    jwt auth filter is added in configuration

--- Backend/src/main/java/com/backend/demo/Config/SecurityConfig.java
+++ Backend/src/main/java/com/backend/demo/Config/SecurityConfig.java
@@ -1,5 +1,7 @@
-package com.backend.demo.config;
+package com.backend.demo.Config;
 
+import com.backend.demo.Security.JwtAuthFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -10,14 +12,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilter(jwtAuthFilter)
                 .authorizeHttpRequests(auth->
                         auth.requestMatchers("/").permitAll());
+
         return httpSecurity.build();
     }
 
