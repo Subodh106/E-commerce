e7f7e9533373f897e6331ffc2b14d8688eec402f
Subodh106 <subodhloves106@gmail.com>
2026-07-07 21:53:23 +0545

    JwtService is done

---
+++ Backend/src/main/java/com/backend/demo/Security/JwtService.java
@@ -0,0 +1,39 @@
+package com.backend.demo.Security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private SecretKey getKey(){
+        String jwtSecret = "JwtSecret";
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateJwtToken(UserDetails userDetails){
+        return  Jwts.builder().subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()*1000*60*15))
+                .signWith(getKey())
+                .compact();
+    }
+
+    public boolean isTokenValid(String email , UserDetails userDetails){
+        return email.equals(userDetails.getUsername());
+    }
+
+    public Claims extractClaims(String Token){
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(Token)
+                .getPayload();
+    }
+}
