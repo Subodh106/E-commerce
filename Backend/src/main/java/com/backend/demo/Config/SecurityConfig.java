ca7547f03feb9ac28cece09c6e1a0ce67b692a3d
Subodh106 <subodhloves106@gmail.com>
2026-07-08 22:54:10 +0545

    Some small refactor

--- Backend/src/main/java/com/backend/demo/Config/SecurityConfig.java
+++ Backend/src/main/java/com/backend/demo/Config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @AllArgsConstructor
 @Configuration
@@ -20,13 +21,12 @@ public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilter(jwtAuthFilter)
                 .authorizeHttpRequests(auth->
-                        auth.requestMatchers("/").permitAll());
-
+                        auth.requestMatchers("/api/v1/auth/**").permitAll());
+        httpSecurity.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
