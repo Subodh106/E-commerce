f0c23628fd5e8c5d1c8f1e198d3999493e2e0cd5
Subodh106 <subodhloves106@gmail.com>
2026-07-08 22:54:00 +0545

    Some small refactor

--- Backend/src/main/java/com/backend/demo/Controller/AuthController.java
+++ Backend/src/main/java/com/backend/demo/Controller/AuthController.java
@@ -1,25 +1,40 @@
 package com.backend.demo.Controller;
 
-import com.backend.demo.Dto.RegisterUserResponseDto;
 import com.backend.demo.Dto.RegisterUserDto;
+import com.backend.demo.Dto.RegisterUserResponseDto;
 import com.backend.demo.Service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/auth/user")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
     public ResponseEntity<RegisterUserResponseDto> RegisterUser(@Valid @RequestBody RegisterUserDto registerUserDto){
-        return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerUser(registerUserDto));
+        String token = authService.registerUser(registerUserDto);
+        ResponseCookie cookie = ResponseCookie.from("jwt",token)
+                .secure(false)
+                .sameSite("Strict")
+                .maxAge(Duration.ofDays(1))
+                .path("/")
+                .httpOnly(true)
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header(HttpHeaders.SET_COOKIE,cookie.toString())
+                .body(new RegisterUserResponseDto("User registered successfully"));
     }
 }
--- Backend/src/main/java/com/backend/demo/Service/AuthService.java
+++ Backend/src/main/java/com/backend/demo/Service/AuthService.java
@@ -1,10 +1,10 @@
 package com.backend.demo.Service;
 
-import com.backend.demo.Dto.RegisterUserResponseDto;
 import com.backend.demo.Dto.RegisterUserDto;
 import com.backend.demo.Entities.Role;
 import com.backend.demo.Entities.User;
 import com.backend.demo.Repository.UserRepository;
+import com.backend.demo.Security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,14 +14,14 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final JwtService jwtService;
 
-    public RegisterUserResponseDto registerUser(RegisterUserDto registerUserDto){
+    public String registerUser(RegisterUserDto registerUserDto){
         if(userRepository.existsByEmail(registerUserDto.getEmail())){
             throw new RuntimeException("Email Already Exist");
         }
         if(userRepository.existsByUsername(registerUserDto.getUsername())){
             throw new RuntimeException("Username Already Exist");
-
         }
         User user = new User();
         user.setEmail(registerUserDto.getEmail());
@@ -29,7 +29,7 @@ public class AuthService {
         user.setUsername(registerUserDto.getUsername());
         user.setPassword(passwordEncoder.encode(registerUserDto.getPassword()));
         user.setRole(Role.USER);
-        User savedUser = userRepository.save(user);
-        return new RegisterUserResponseDto(savedUser.getId(),savedUser.getUsername(),savedUser.getEmail(),savedUser.getRole());
+        userRepository.save(user);
+        return jwtService.generateJwtToken(user);
     }
 }
