2d230efe5dd30a09cb7576392757f5602741dd6f
Subodh106 <subodhloves106@gmail.com>
2026-07-08 20:54:28 +0545

    UserRepository is make and some code

--- Backend/src/main/java/com/backend/demo/Repository/UserRepository.java
+++ Backend/src/main/java/com/backend/demo/Repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.backend.demo.Repository;
 
-public class UserRepository {
+import com.backend.demo.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+boolean existsByEmail(String email);
+boolean existsByUsername(String username);
+
 }
