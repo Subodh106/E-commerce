210f4ed18b8571d32619f17198611de06115c8bd
Subodh106 <subodhloves106@gmail.com>
2026-07-08 20:53:38 +0545

    RegisterUserDto is done

---
+++ Backend/src/main/java/com/backend/demo/Dto/RegisterUserDto.java
@@ -0,0 +1,18 @@
+package com.backend.demo.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RegisterUserDto {
+    @NotBlank(message="Username can't be empty")
+    private String username;
+    @NotBlank(message="Email can't be empty")
+    private String email;
+    @NotBlank(message="Password can't be empty")
+    private String password;
+}
