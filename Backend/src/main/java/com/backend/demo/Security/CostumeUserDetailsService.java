abf920c7b36f7a872a1416cba60b27f826d1f54e
Subodh106 <subodhloves106@gmail.com>
2026-07-09 18:07:08 +0545

    Custom exception handler

--- Backend/src/main/java/com/backend/demo/Security/CostumeUserDetailsService.java
+++ Backend/src/main/java/com/backend/demo/Security/CostumeUserDetailsService.java
@@ -1,8 +1,10 @@
 package com.backend.demo.Security;
 
+import com.backend.demo.Exception.Custom.ResourceNotFoundException;
 import com.backend.demo.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
@@ -13,12 +15,9 @@ public class CostumeUserDetailsService implements UserDetailsService {
     private  final UserRepository userRepository;
 
     @Override
-    @lombok.NonNull
-    public UserDetails loadUserByUsername(@NonNull String email) {
-
+    public @Nullable UserDetails loadUserByUsername(@NonNull String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() ->
-                        new RuntimeException("User not found"));
+                .orElseThrow(()->new ResourceNotFoundException("User doesn't exist with "+email));
     }
 }
 
