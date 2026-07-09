6e1615991db9b90c8f13c58628b1a3450a8b0d3f
Subodh106 <subodhloves106@gmail.com>
2026-07-09 20:09:51 +0545

    RegisterUserResponseDto refactor

--- Backend/src/main/java/com/backend/demo/Dto/RegisterUserResponseDto.java
+++ Backend/src/main/java/com/backend/demo/Dto/RegisterUserResponseDto.java
@@ -1,16 +1,16 @@
 package com.backend.demo.Dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class RegisterUserResponseDto {
     private String message;
+    private UserResponseDto user;
 
-    public RegisterUserResponseDto() {
-
+    public RegisterUserResponseDto(String message , UserResponseDto user){
+        this.message=message;
+        this.user = user;
     }
 }
