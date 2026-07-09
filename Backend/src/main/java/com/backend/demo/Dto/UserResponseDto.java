d8857ac816c47b4d0efbfe806451c107c75ec3af
Subodh106 <subodhloves106@gmail.com>
2026-07-09 20:10:04 +0545

    UserResponseDto refactor

---
+++ Backend/src/main/java/com/backend/demo/Dto/UserResponseDto.java
@@ -0,0 +1,16 @@
+package com.backend.demo.Dto;
+
+import com.backend.demo.Entities.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String email;
+    private Role role;
+}
