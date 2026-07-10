d8e6512dc2b0c66f265ada441aec9b229aba5360
Subodh106 <subodhloves106@gmail.com>
2026-07-10 08:48:07 +0545

    fix: In GlobalExceptionHandler ExceptionResponseDto is implemented so that exception can be thrown in proper format

--- Backend/src/main/java/com/backend/demo/Exception/Handler/GlobalExceptionHandler.java
+++ Backend/src/main/java/com/backend/demo/Exception/Handler/GlobalExceptionHandler.java
@@ -1,42 +1,67 @@
 package com.backend.demo.Exception.Handler;
 
-import com.backend.demo.Exception.Custom.EmailAlreadyExistException;
+import com.backend.demo.Dto.exception.ExceptionResponseDto;
 import com.backend.demo.Exception.Custom.InvalidCredentialsException;
 import com.backend.demo.Exception.Custom.ResourceNotFoundException;
-import com.backend.demo.Exception.Custom.UsernameAlreadyExistException;
+import com.backend.demo.Exception.Custom.DuplicatedValueException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException ex){
+    public ResponseEntity<ExceptionResponseDto> handleRuntimeException(RuntimeException ex, HttpServletRequest request){
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ex.getMessage());
+                .body(exceptionResponseDto);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
+    public ResponseEntity<ExceptionResponseDto> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request){
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+                .body(exceptionResponseDto);
     }
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception ex){
+    public ResponseEntity<ExceptionResponseDto> handleGenericException(Exception ex , HttpServletRequest request){
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Something went wrong. Please try again later");
+                .body(exceptionResponseDto);
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String ,String>> handleValidationException(MethodArgumentNotValidException ex){
-        Map<String ,String > errors = new HashMap<String , String>();
+        Map<String ,String > errors = new HashMap<>();
         ex.getBindingResult()
                 .getFieldErrors()
                 .forEach(error ->
@@ -47,23 +72,32 @@ public class GlobalExceptionHandler {
                 );
         return ResponseEntity.badRequest().body(errors);
     }
-    @ExceptionHandler(EmailAlreadyExistException.class)
-    public ResponseEntity<String> handleEmailAlreadyExistException(Exception ex){
-        return  ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(ex.getMessage());
-    }
     @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<String> handleInvalidCredentialsException(Exception ex){
+    public ResponseEntity<ExceptionResponseDto> handleInvalidCredentialsException(Exception ex,HttpServletRequest request){
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(ex.getMessage());
+                .body(exceptionResponseDto);
     }
 
-    @ExceptionHandler(UsernameAlreadyExistException.class)
-    public ResponseEntity<String> handleUsernameAlreadyExistException(Exception ex){
+    @ExceptionHandler(DuplicatedValueException.class)
+    public ResponseEntity<ExceptionResponseDto> handleDuplicatedValueException(Exception ex,HttpServletRequest request){
+        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
-                .body(ex.getMessage());
+                .body(exceptionResponseDto);
     }
 }
