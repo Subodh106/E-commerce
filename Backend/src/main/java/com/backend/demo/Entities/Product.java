218b52e9aad399a3c727d3cca3e2280254e154c1
Subodh106 <subodhloves106@gmail.com>
2026-07-10 08:48:59 +0545

    New : New product entity is created to build new feature of e-commerce platform

---
+++ Backend/src/main/java/com/backend/demo/Entities/Product.java
@@ -0,0 +1,53 @@
+package com.backend.demo.Entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    @Positive
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Min(0)
+    private int stock;
+
+    @Column(nullable = false)
+    @ManyToAny
+    private String category;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
