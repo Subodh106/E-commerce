594f902d912dad503b7ed0cca751135e55431bf3
Subodh106 <subodhloves106@gmail.com>
2026-07-11 16:16:21 +0545

    New : Category entity is added to handle the different category of product

---
+++ Backend/src/main/java/com/backend/demo/Entities/Category.java
@@ -0,0 +1,21 @@
+package com.backend.demo.Entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @OneToMany(mappedBy = "category")
+    private List<Product> productList = new ArrayList<>();
+}
