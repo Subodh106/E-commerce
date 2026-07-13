3c4f8b73eab79841e757e00e87c776c26f7a13a3
Subodh106 <subodhloves106@gmail.com>
2026-07-13 21:57:17 +0545

    feat : get all product end point is created

--- Backend/src/main/java/com/backend/demo/Controller/ProductController.java
+++ Backend/src/main/java/com/backend/demo/Controller/ProductController.java
@@ -3,17 +3,18 @@ package com.backend.demo.Controller;
 import com.backend.demo.Common.ApiResponse;
 import com.backend.demo.Dto.Product.ProductRequestDto;
 import com.backend.demo.Dto.Product.ProductResponseDto;
+import com.backend.demo.Repository.ProductRepository;
 import com.backend.demo.Security.CustomUserDetails;
 import com.backend.demo.Service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -28,5 +29,11 @@ public class ProductController {
 
         ApiResponse<ProductResponseDto> productResponse = new ApiResponse<ProductResponseDto>("Product Created Successfully", response);
         return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse<List<ProductResponseDto>>> getAllProducts(@RequestParam(defaultValue = "10")int size ,@RequestParam(defaultValue = "0") int page , @RequestParam(defaultValue = "asc") String direction , @RequestParam(defaultValue = "name") String sortBy){
+        List<ProductResponseDto> response = productService.getAllProduct(size , page , direction , sortBy);
+        ApiResponse<List<ProductResponseDto>> productResponse = new ApiResponse<List<ProductResponseDto>>("All product retrived successfully",response);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponse);
     }
 }
