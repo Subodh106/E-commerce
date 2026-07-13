1cd205cb09c9d3e5b4a5f85ca9542cbcbcd6dfb1
Subodh106 <subodhloves106@gmail.com>
2026-07-13 19:19:17 +0545

    feat : CustomUserDetails is created

---
+++ Backend/src/main/java/com/backend/demo/Security/CustomUserDetails.java
@@ -0,0 +1,30 @@
+package com.backend.demo.Security;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+
+public class CustomUserDetails implements UserDetails {
+    private Long id;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+}
