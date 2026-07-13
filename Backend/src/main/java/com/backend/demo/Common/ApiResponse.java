630a0dd8889c564fe0c3186e3a4a3299c1225519
Subodh106 <subodhloves106@gmail.com>
2026-07-13 19:16:50 +0545

    feat : create reusable ApiResponse wrapper

---
+++ Backend/src/main/java/com/backend/demo/Common/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.backend.demo.Common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private String message;
+    private T data;
+}
