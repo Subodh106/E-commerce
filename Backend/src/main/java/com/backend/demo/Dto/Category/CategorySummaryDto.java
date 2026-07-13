dbd1d32ca4226fa1ca7ba0dfba47611edc9577da
Subodh106 <subodhloves106@gmail.com>
2026-07-13 21:26:10 +0545

    removed

--- Backend/src/main/java/com/backend/demo/Dto/Category/CategorySummaryDto.java
+++ Backend/src/main/java/com/backend/demo/Dto/Category/CategorySummaryDto.java
@@ -1,17 +1,13 @@
 package com.backend.demo.Dto.Category;
 
-import com.backend.demo.Dto.Product.ProductResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class CategoryDto {
+public class CategorySummaryDto {
     private Long id;
     private String name;
 }
