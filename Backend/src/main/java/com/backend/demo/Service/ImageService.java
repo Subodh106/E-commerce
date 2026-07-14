dc317840259d606a3ccd8ba8eac88091846d85eb
Subodh106 <subodhloves106@gmail.com>
2026-07-14 20:40:36 +0545

    feat: ImageService is created for image upload

---
+++ Backend/src/main/java/com/backend/demo/Service/ImageService.java
@@ -0,0 +1,28 @@
+package com.backend.demo.Service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class ImageService {
+    private final Cloudinary cloudinary;
+
+    public String upload(MultipartFile file) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(
+                file.getBytes(),
+                ObjectUtils.emptyMap()
+        );
+        return uploadResult.get("secure_url").toString();
+    }
+}
