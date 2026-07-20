17119ac9ff3d18d42e19624d5c15eb747b945061
Subodh106 <subodhloves106@gmail.com>
2026-07-20 12:53:19 +0545

    feat(Image): ImageDto is created

---
+++ Backend/src/main/java/com/backend/demo/Dto/Image/ImageDto.java
@@ -0,0 +1,13 @@
+package com.backend.demo.Dto.Image;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ImageDto {
+    String imageUrl;
+    String publicID;
+}
