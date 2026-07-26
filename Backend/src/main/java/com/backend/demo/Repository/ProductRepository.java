c518cbe7c1b285891d43a02b972218a09b4dee28
Subodh106 <subodhloves106@gmail.com>
2026-07-26 12:17:17 +0545

    feat(Product): specification is added in repository layer

--- Backend/src/main/java/com/backend/demo/Repository/ProductRepository.java
+++ Backend/src/main/java/com/backend/demo/Repository/ProductRepository.java
@@ -4,8 +4,9 @@ import com.backend.demo.Entities.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findByNameContainingIgnoreCase(String search , Pageable pageable);
+public interface ProductRepository extends JpaRepository<Product, Long> , JpaSpecificationExecutor<Product> {
+    Page<Product> findByNameContainingIgnoreCase(String search, Pageable pageable);
 }
