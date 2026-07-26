5c1af466235e320cb07a19dbb6f15b05bc83bf3f
Subodh106 <subodhloves106@gmail.com>
2026-07-26 11:19:02 +0545

    feat(Product): derived queries

--- Backend/src/main/java/com/backend/demo/Repository/ProductRepository.java
+++ Backend/src/main/java/com/backend/demo/Repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.backend.demo.Repository;
 
 import com.backend.demo.Entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByNameContainingIgnoreCase(String search , Pageable pageable);
 }
