6ef6e1b41038cc78f36c644cbdc6b56b7ba24124
Subodh106 <subodhloves106@gmail.com>
2026-08-23 21:25:40 +0545

    feat(component): update Navbar layout and improve search functionality

--- frontend/components/web/navbar.tsx
+++ frontend/components/web/navbar.tsx
@@ -1,31 +1,48 @@
 import { Input } from '@base-ui/react'
 import Link from 'next/link'
-import { Button } from '../ui/button'
-import {Search} from "lucide-react"
+import { Heart, Search, ShoppingCart, User} from "lucide-react"
 import { ModeToggle } from './ModeToggle'
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between px-3 py-1 border'>
+    <nav className='flex items-center justify-between  w-full px-2 m-2'>       
         <div>
-            <div className='text-2xl'>
-                E-Commerce
+            <div className='text-sm md:text-xl lg:text-2xl'>
+                <Link href="/home" className='font-bold'>
+                    <span className='text-sm md:text-xl lg:text-2xl text-blue-500'>R</span>
+                    <span className='text-xs md:text-sm lg:text-sm'>AY SHOP</span>
+                </Link>
             </div>
         </div>
-        <div className='border border-black rounded-sm p-1 flex items-center justify-between' >
-            <Input placeholder='Search' className="outline-none"/>
-            <Search/>
+        <div className=' flex justify-between items-center lg:gap-10 sm:gap-2 text-sm md:text-lg lg:text-xl' >
+            <Link href="/home" >
+                Home
+            </Link>
+            <Link href="/shop">
+                Shop
+            </Link>
+            <Link href="/categories">
+                Categories
+            </Link>
+            <Link href="/about">
+                About
+            </Link>
+            <Link href="/contact">
+                Contact
+            </Link>
         </div>
         <div className='flex justify-center items-center gap-3'>
-            <ModeToggle/>
-            <Link href="/auth/log-in">
-                <Button className="text-center cursor-pointer text-xl p-3" variant="outline">lOG IN</Button>
-            </Link>
-            <Link href="/auth/sign-up">
-                <Button className="text-center cursor-pointer text-xl p-3">
-                    SIGN UP 
-                </Button>
-            </Link>
+            <div className='flex justify-between items-center gap-3 border border-gray-300 rounded-sm shadow-2xl p-1 bg-gray-100'>
+                <Search className='cursor-pointer'/>
+                <Input 
+                    placeholder='Search products'
+                    className="outline-none"
+                />
+            </div>
+            <ModeToggle />
+            <User className='cursor-pointer'/>
+            <Heart className='cursor-pointer'/>
+            <ShoppingCart className='cursor-pointer'/>
         </div>
     </nav>
   )
