029d467fb48fdb5bad3f505c2de663595967242a
Subodh106 <subodhloves106@gmail.com>
2026-08-24 13:01:59 +0545

    chore: update lucide-react to version 1.33.0 in package.json

--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -1,10 +1,13 @@
-import Navbar from "@/components/web/navbar"
+import { Footer } from "@/components/web/Footer";
+import { Navbar } from "@/components/web/navbar";
+
 
 export default  function homeLayout  ({children}:{children:React.ReactNode}){
     return(
         <>
         <Navbar/>
         {children}
+        <Footer/>
         </>
     )
 }
