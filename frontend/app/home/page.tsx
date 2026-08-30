cc900e3a29dddddd5e563103ff514ca1057b53e0
Subodh106 <subodhloves106@gmail.com>
2026-08-30 19:09:05 +0545

    feat(component): refactor page component by removing unused imports and axios fetch function

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,19 +1,11 @@
-"use client"
 import { CategorySection } from "@/components/web/category-section"
 import { FeaturedProductsSection } from "@/components/web/featured-products-section"
-
 import { FeaturesSection } from "@/components/web/feature-section"
 import { HeroSection } from "@/components/web/hero-section"
 import { NewsletterSection } from "@/components/web/news-letter-section"
 import { Separator } from "@base-ui/react"
-import axios from "axios"
 
-const page = () => {
-
-  const fetchProduct =async()=>{
-      const response = await axios.get(`${process.env.NEXT_PUBLIC_SPRING_API_URL}/products`)
-  }
-
+const  page = async() => {
   return (
     <div className="flex gap-3 flex-col ">
       <HeroSection/>
@@ -24,6 +16,7 @@ const page = () => {
       <NewsletterSection/>
     </div>
   )
+
 }
 
 export default page
