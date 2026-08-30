a3be007d4e75a0faac7d172e88e0b16757398d67
Subodh106 <subodhloves106@gmail.com>
2026-08-30 19:09:15 +0545

    feat(layout): update homeLayout to use async function and add missing imports

--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -1,8 +1,11 @@
+"use client"
 import { Footer } from "@/components/web/footer";
 import { Navbar } from "@/components/web/navbar";
+import { getCookies } from "@/utils/getCookies";;
+import { useRouter } from "next/router";
 
 
-export default  function homeLayout  ({children}:{children:React.ReactNode}){
+export default async function homeLayout  ({children}:{children:React.ReactNode}){
     return(
         <>
         <Navbar/>
