15cad7b839d091d58116668ee887e9611d5d768d
Subodh106 <subodhloves106@gmail.com>
2026-08-30 19:11:25 +0545

    feat(user): implement UserService to retrieve user details by ID

--- Backend/src/main/java/com/backend/demo/Service/UserService.java
+++ Backend/src/main/java/com/backend/demo/Service/UserService.java
@@ -1,4 +1,22 @@
 package com.backend.demo.Service;
 
+import com.backend.demo.Dto.User.UserResponseDto;
+import com.backend.demo.Entities.User;
+import com.backend.demo.Exception.Custom.ResourceNotFoundException;
+import com.backend.demo.Repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserResponseDto getMe(Long id){
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        return new UserResponseDto(user.getId(),user.getEmail(),user.getUsername(),user.getRole());
+    }
+
 }
