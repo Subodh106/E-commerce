83ac452883f359e9697c66c41c0be19173ecaa8f
Subodh106 <subodhloves106@gmail.com>
2026-09-08 13:39:29 +0545

    fix(layout): remove "use client" directive from homeLayout function

--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -1,10 +1,10 @@
-"use client"
+
 import { Footer } from "@/components/web/footer";
 import { Navbar } from "@/components/web/navbar";
 import { useRouter } from "next/router";
 
 
-export default async function homeLayout  ({children}:{children:React.ReactNode}){
+export default function homeLayout  ({children}:{children:React.ReactNode}){
     return(
         <>
         <Navbar/>
