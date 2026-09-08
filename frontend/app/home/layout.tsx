2c7519d578335ae0e2d67e4fa646b1c14194b31e
Subodh106 <subodhloves106@gmail.com>
2026-09-08 21:05:26 +0545

    fix(layout): remove unused useRouter import from homeLayout component

--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -1,7 +1,6 @@
 
 import { Footer } from "@/components/web/footer";
 import { Navbar } from "@/components/web/navbar";
-import { useRouter } from "next/router";
 
 
 export default function homeLayout  ({children}:{children:React.ReactNode}){
