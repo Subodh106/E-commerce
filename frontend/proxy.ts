953f2009f25db13ebaabfa0f667e857db336da97
Subodh106 <subodhloves106@gmail.com>
2026-09-08 21:04:58 +0545

    fix(proxy): streamline authentication checks and improve redirect logic

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,20 +1,31 @@
 "use server"
 import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+import { isatty } from "tty";
 
 export function proxy(request : NextRequest){
     const {pathname} = request.nextUrl;
     const isAuthed = request.cookies.get("token")?.value=="1";
+    console.log(request.cookies.get("token"))
   
-    if(pathname.startsWith("/home")){
-        if(!isAuthed){
+    if(pathname.startsWith("/home") && !isAuthed){
             console.log("not home")
             const loginUrl = request.nextUrl.clone();
             loginUrl.pathname="/auth/log-in";
             loginUrl.searchParams.set("next" ,pathname);
             return NextResponse.redirect(loginUrl);
-        }
+      
     }
-    if(pathname==="/auth/log-in" && isAuthed){
+    if(pathname ==="/products" && !isAuthed){
+      
+            console.log(request.cookies.get("token"));
+            console.log("products");
+            const productsUrl = request.nextUrl.clone();
+            productsUrl.pathname = "/auth/log-in";
+            productsUrl.searchParams.set("next",pathname);
+            return NextResponse.redirect(productsUrl);
+    }
+    if(pathname==="/auth/log-in" && !isAuthed){
         console.log("home")
         const home = request.nextUrl.clone();
         home.pathname = "/home";
@@ -25,5 +36,5 @@ export function proxy(request : NextRequest){
 }
 
 export const config={
-    matcher:["/home/:path","/auth/log-in/:path"]
+    matcher:["/home/:path","/auth/log-in"]
 }
