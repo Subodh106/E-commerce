76a48ed4bc0c24fe68bb5133dfac325fecfd6ad7
Subodh106 <subodhloves106@gmail.com>
2026-09-10 13:57:50 +0545

    feat(search): implement controlled input for search functionality

--- frontend/components/web/search.tsx
+++ frontend/components/web/search.tsx
@@ -1,16 +1,21 @@
 import { Search } from 'lucide-react'
-import React from 'react'
 
-const SearchItem = () => {
+const SearchItem = ({search , setSearch}:{search:string , setSearch:(value:string)=>void}) => {
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    setSearch(e.target.value);
+  }
+  
   return (
     <div className="relative mb-6">
                 <Search
                   size={18}
                   className="absolute left-3 top-1/2 -translate-y-1/2 text-slate-400"
                 />
-    
                 <input
                   type="text"
+                  value={search}
+                  onChange={handleChange}
                   placeholder="Search products..."
                   className="w-full rounded-lg border border-slate-200 py-3 pl-10 pr-4 text-sm outline-none focus:border-slate-900"
                 />
