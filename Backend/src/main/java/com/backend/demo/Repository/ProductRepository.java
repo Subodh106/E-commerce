7a0ecbe38f966e751a758d6edb049788a368ac0f
Subodh106 <subodhloves106@gmail.com>
2026-09-13 18:05:37 +0545

    fix(repository): add findAll method to ProductRepository for pagination and specification support

--- Backend/src/main/java/com/backend/demo/Repository/ProductRepository.java
+++ Backend/src/main/java/com/backend/demo/Repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.backend.demo.Repository;
 
 import com.backend.demo.Entities.Product;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long> , JpaSpecificationExecutor<Product> {
+  Page<Product> findAll(@NonNull Specification<Product> specification, @NonNull Pageable pageable);
 }
