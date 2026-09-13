67aebe8ed6c83e060beecab424554a198dac0b03
Subodh106 <subodhloves106@gmail.com>
2026-09-13 18:05:31 +0545

    fix(security): update product and logout endpoints to require authentication

--- Backend/src/main/java/com/backend/demo/Config/SecurityConfig.java
+++ Backend/src/main/java/com/backend/demo/Config/SecurityConfig.java
@@ -30,8 +30,8 @@ public class SecurityConfig {
                     .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource))
                     .csrf(AbstractHttpConfigurer::disable)
                     .authorizeHttpRequests(auth-> auth.requestMatchers("/api/v1/auth/**").permitAll()
-                            .requestMatchers("/api/v1/auth/logout").permitAll()
-                            .requestMatchers(HttpMethod.GET ,"/api/v1/products","/api/v1/products/**").permitAll()
+                            .requestMatchers("/api/v1/auth/logout").authenticated()
+                            .requestMatchers(HttpMethod.GET ,"/api/v1/products","/api/v1/products/**").authenticated()
                             .anyRequest().authenticated()
                         );
         httpSecurity.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
