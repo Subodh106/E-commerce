0051f9acfe4422420ab370af3442689c06f3c565
Subodh106 <subodhloves106@gmail.com>
2026-09-15 09:03:48 +0545

    feat(types): add User type for user representation in the application

--- frontend/Types/authTypes.ts
+++ frontend/Types/authTypes.ts
@@ -8,3 +8,11 @@ export type SignUpUser = {
     username : string , 
     password : string , 
 }
+
+export type User = {
+    id:string,
+    email:string ,
+    username : string
+    role :string
+}
+
