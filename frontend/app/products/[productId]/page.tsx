99c0c5a1284d08598c5e47f27877aa8380662341
Subodh106 <subodhloves106@gmail.com>
2026-09-15 08:20:00 +0545

    feat(product): implement product details fetching and error handling

--- frontend/app/products/[productId]/page.tsx
+++ frontend/app/products/[productId]/page.tsx
@@ -1,9 +1,13 @@
 'use client';
 
-import { act, useState } from 'react';
-import { Star, Heart, ShoppingCart, Minus, Plus, Truck, ShieldCheck, RefreshCw } from 'lucide-react';
+import {useEffect, useState } from 'react';
 import LeftSection from '@/components/web/product-details-components/left-section';
 import Rightsection from '@/components/web/product-details-components/right-section';
+import { useRouter } from 'next/navigation';
+import { useParams } from 'next/navigation';
+import axios from 'axios';
+import { toast, Toaster } from 'sonner';
+import { ProductType } from '@/Types/HomeTypes';
 
 const images = [
   'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=800&auto=format&fit=crop&q=60',
@@ -19,10 +23,32 @@ const colors = [
 ];
 
 export default function ProductDetails() {
+  const[productData , setProductData] = useState<ProductType>();
   const [selectedImage, setSelectedImage] = useState<number>(0);
   const [selectedColor, setSelectedColor] = useState<string>('Black');
   const [quantity, setQuantity] = useState<number>(1);
   const [activeTab, setActiveTab] = useState<string>('Description');
+  const [serverErrors , setServerErrors] = useState<string>("");
+
+  const params = useParams<{ productId: string }>();
+  const productId = params.productId;
+  const router = useRouter();
+
+  useEffect(()=>{
+    getProductDetails();
+  },[])
+
+  const getProductDetails = async()=>{
+    
+      try {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL}/products/${productId}` ,{withCredentials:true})
+        console.log(res)
+      } catch (error:any) {
+        setServerErrors(error?.response?.data?.message)
+        toast.error(error?.response?.data?.message)
+        router.push("/products")
+      }
+  }
 
   return (
     <div className="max-w-7xl mx-auto px-4 py-8 bg-white dark:bg-black text-zinc-900 dark:text-zinc-100 transition-colors duration-200">
