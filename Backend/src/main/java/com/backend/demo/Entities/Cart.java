4d424fee624b00cdb37a1cbfb5800a8ac2598a1c
Subodh106 <subodhloves106@gmail.com>
2026-09-16 20:21:01 +0545

    feat(cart): implement Cart entity with userId, cartItem, and timestamps

--- Backend/src/main/java/com/backend/demo/Entities/Cart.java
+++ Backend/src/main/java/com/backend/demo/Entities/Cart.java
@@ -1,4 +1,32 @@
 package com.backend.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private CartItem cartItem;
+
+    @Column(nullable = false)
+    private Date created_at;
+
+    @Column(nullable = false)
+    private Date updated_at;
 }
