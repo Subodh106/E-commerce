b581a7aaeb7b923b55be2150d7bf9c26bacd44a2
Subodh106 <subodhloves106@gmail.com>
2026-09-17 13:09:38 +0545

    feat(cart): enhance CartResponseDto with additional fields for cart details

--- Backend/src/main/java/com/backend/demo/Dto/Cart/CartResponseDto.java
+++ Backend/src/main/java/com/backend/demo/Dto/Cart/CartResponseDto.java
@@ -1,4 +1,20 @@
 package com.backend.demo.Dto.Cart;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class CartResponseDto {
+    private Long id;
+    private Long user_id;
+    private CartItemDto cartItems;
+    private Date create_at;
+    private Date updated_at;
 }
