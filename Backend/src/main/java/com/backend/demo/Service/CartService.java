a20508689967be7d943418c83b1d55faff8344ad
Subodh106 <subodhloves106@gmail.com>
2026-09-17 16:07:19 +0545

    feat(cart): implement addToCart method in CartService to retrieve cart by userId

--- Backend/src/main/java/com/backend/demo/Service/CartService.java
+++ Backend/src/main/java/com/backend/demo/Service/CartService.java
@@ -1,4 +1,23 @@
 package com.backend.demo.Service;
 
+import com.backend.demo.Dto.Cart.CartItemDto;
+import com.backend.demo.Dto.Cart.CartResponseDto;
+import com.backend.demo.Entities.Cart;
+import com.backend.demo.Repository.CartRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CartService {
+    
+    private final CartRepository cartRepository;
+
+    public Optional<Cart> addToCart(CartItemDto cartItemDto , Long userId){
+        return cartRepository.findByUserId(userId);
+    }
+
 }
