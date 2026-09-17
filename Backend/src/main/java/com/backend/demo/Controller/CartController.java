94c1283c7a0101a8668f492797af6d70e6ae35ad
Subodh106 <subodhloves106@gmail.com>
2026-09-17 13:10:07 +0545

    feat(cart): implement CartController and add CartItemDto structure

--- Backend/src/main/java/com/backend/demo/Controller/CartController.java
+++ Backend/src/main/java/com/backend/demo/Controller/CartController.java
@@ -1,4 +1,18 @@
 package com.backend.demo.Controller;
 
+import com.backend.demo.Common.ApiResponse;
+import com.backend.demo.Dto.Cart.CartResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/cart")
 public class CartController {
+
+//    @PostMapping
+//    public ResponseEntity<ApiResponse<CartResponseDto>> addToCart(){
+//        return new ApiResponse<>("Cart created successfully",{});
+//    }
 }
--- Backend/src/main/java/com/backend/demo/Dto/Cart/CartItemDto.java
+++ Backend/src/main/java/com/backend/demo/Dto/Cart/CartItemDto.java
@@ -1,4 +1,10 @@
 package com.backend.demo.Dto.Cart;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 public class CartItemDto {
+
 }
