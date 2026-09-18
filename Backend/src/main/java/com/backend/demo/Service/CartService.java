c49df38774f8b4b4c7980efcfe2a8edbeafa99ec
Subodh106 <subodhloves106@gmail.com>
2026-09-18 20:31:51 +0545

    feat(cart): update addToCart method to return Cart object and handle product retrieval

--- Backend/src/main/java/com/backend/demo/Service/CartService.java
+++ Backend/src/main/java/com/backend/demo/Service/CartService.java
@@ -1,13 +1,15 @@
 package com.backend.demo.Service;
 
 import com.backend.demo.Dto.Cart.CartItemDto;
-import com.backend.demo.Dto.Cart.CartResponseDto;
 import com.backend.demo.Entities.Cart;
+import com.backend.demo.Entities.Product;
+import com.backend.demo.Exception.Custom.ResourceNotFoundException;
 import com.backend.demo.Repository.CartRepository;
-import lombok.AllArgsConstructor;
+import com.backend.demo.Repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -15,9 +17,17 @@ import java.util.Optional;
 public class CartService {
     
     private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
 
-    public Optional<Cart> addToCart(CartItemDto cartItemDto , Long userId){
-        return cartRepository.findByUserId(userId);
+    public Cart addToCart(CartItemDto cartItemDto , Long userId){
+        Optional<Cart> existedCart=  cartRepository.findByUserId(userId);
+        Product product = productRepository.findById(cartItemDto.getProductId()).orElseThrow(()->new ResourceNotFoundException("Product is not found"));
+        Cart newCart = new Cart();
+        newCart.setUserId(userId);
+        newCart.setCartItems(product.getId(),product.getCategory(),product.getPrice(),cartItemDto.getQuantity(),new Date());
+        newCart.setCreated_at(new Date());
+        newCart.setUpdated_at(new Date());
+    return newCart;
     }
 
 }
