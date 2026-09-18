206be3bcc0129c69c7e53a78d23a7445ac34589a
Subodh106 <subodhloves106@gmail.com>
2026-09-18 20:31:36 +0545

    feat(cart): update CartResponseDto to include product details and pricing information

--- Backend/src/main/java/com/backend/demo/Dto/Cart/CartResponseDto.java
+++ Backend/src/main/java/com/backend/demo/Dto/Cart/CartResponseDto.java
@@ -1,10 +1,12 @@
 package com.backend.demo.Dto.Cart;
 
+import com.backend.demo.Dto.Category.CategorySummaryDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @AllArgsConstructor
@@ -14,7 +16,10 @@ import java.util.Date;
 public class CartResponseDto {
     private Long id;
     private Long user_id;
-    private CartItemDto cartItems;
+    private Long product_id;
+    private CategorySummaryDto categorySummaryDto;
+    private BigDecimal price;
+    private int quantity;
     private Date create_at;
     private Date updated_at;
 }
