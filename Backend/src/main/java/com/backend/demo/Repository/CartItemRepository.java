f9887136c30011d775e52ae08449196d3ed876e7
Subodh106 <subodhloves106@gmail.com>
2026-09-20 11:41:15 +0545

    feat(cart): implement CartItemRepository interface extending JpaRepository for CartItem

--- Backend/src/main/java/com/backend/demo/Repository/CartItemRepository.java
+++ Backend/src/main/java/com/backend/demo/Repository/CartItemRepository.java
@@ -1,4 +1,9 @@
 package com.backend.demo.Repository;
 
-public interface CartItemRepository {
+import com.backend.demo.Entities.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem , Long> {
 }
