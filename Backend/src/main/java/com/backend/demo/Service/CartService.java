31576621ff48df7831a18fccf77bc991d86714d9
Subodh106 <subodhloves106@gmail.com>
2026-09-20 11:41:19 +0545

    feat(cart): refactor addToCart method to handle cart creation and return CartResponseDto

--- Backend/src/main/java/com/backend/demo/Service/CartService.java
+++ Backend/src/main/java/com/backend/demo/Service/CartService.java
@@ -1,33 +1,88 @@
 package com.backend.demo.Service;
 
 import com.backend.demo.Dto.Cart.CartItemDto;
+import com.backend.demo.Dto.Cart.CartItemResponseDto;
+import com.backend.demo.Dto.Cart.CartResponseDto;
 import com.backend.demo.Entities.Cart;
+import com.backend.demo.Entities.CartItem;
 import com.backend.demo.Entities.Product;
 import com.backend.demo.Exception.Custom.ResourceNotFoundException;
 import com.backend.demo.Repository.CartRepository;
 import com.backend.demo.Repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
-    
+
     private final CartRepository cartRepository;
     private final ProductRepository productRepository;
 
-    public Cart addToCart(CartItemDto cartItemDto , Long userId){
-        Optional<Cart> existedCart=  cartRepository.findByUserId(userId);
-        Product product = productRepository.findById(cartItemDto.getProductId()).orElseThrow(()->new ResourceNotFoundException("Product is not found"));
-        Cart newCart = new Cart();
-        newCart.setUserId(userId);
-        newCart.setCartItems(product.getId(),product.getCategory(),product.getPrice(),cartItemDto.getQuantity(),new Date());
-        newCart.setCreated_at(new Date());
-        newCart.setUpdated_at(new Date());
-    return newCart;
+    @Transactional
+    public CartResponseDto addToCart(CartItemDto cartItemDto , Long userId){
+        Cart existedCart=  cartRepository.findByUserId(userId).orElseGet(
+                ()->{
+                    Cart newCart = new Cart();
+                    newCart.setUserId(userId);
+                    newCart.setCreated_at(new Date());
+                    return newCart;
+                }
+        );
+
+        Product product = productRepository.findById(
+                cartItemDto.getProductId()).orElseThrow(()->
+                    new ResourceNotFoundException("Product is not found")
+                );
+        if(existedCart.getCartItems()==null){
+            existedCart.setCartItems(new ArrayList<>());
+        }
+
+        CartItem cartItem = new CartItem();
+        cartItem.setProductId(product.getId());
+        cartItem.setCategory(product.getCategory());
+        cartItem.setPrice(product.getPrice());
+        cartItem.setImageUrl(product.getImageUrl());
+        cartItem.setQuantity(cartItemDto.getQuantity());
+        cartItem.setCart(existedCart);
+
+        existedCart.setUserId(userId);
+        List<CartItem> cartItemList = existedCart.getCartItems();
+        cartItemList.add(cartItem);
+        existedCart.setCartItems(cartItemList);
+        existedCart.setUpdated_at(new Date());
+        Cart savedCart = cartRepository.save(existedCart);
+        return buildCartDto(savedCart);
+    }
+
+    private CartResponseDto buildCartDto(Cart cart){
+        CartResponseDto cartResponseDto = new CartResponseDto();
+        cartResponseDto.setId(cart.getId());
+        List<CartItemResponseDto> cartItemsList = new ArrayList<>();
+        if(cart.getCartItems()!=null){
+        for(CartItem cartItems : cart.getCartItems()){
+            CartItemResponseDto dto = new CartItemResponseDto();
+            dto.setId(cartItems.getId());
+            dto.setProductId(cartItems.getProductId());
+            dto.setCategory(cartItems.getCategory());
+            dto.setPrice(cartItems.getPrice());
+            dto.setImageUrl(cartItems.getImageUrl());
+            dto.setQuantity(cartItems.getQuantity());
+            dto.setCart(cartItems.getCart());
+            dto.setAdded_at(cartItems.getAdded_at());
+            cartItemsList.add(dto);
+        }
+        }
+        cartResponseDto.setCartItems(cartItemsList);
+        cartResponseDto.setUser_id(cart.getUserId());
+        cartResponseDto.setCreate_at(cart.getCreated_at());
+        cartResponseDto.setUpdated_at(cart.getUpdated_at());
+        return cartResponseDto;
     }
 
 }
