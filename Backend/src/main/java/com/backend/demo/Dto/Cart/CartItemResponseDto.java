3af04ea62db2be7c38850b8b62023c396d3f867a
Subodh106 <subodhloves106@gmail.com>
2026-09-20 11:40:58 +0545

    feat(cart): enhance CartItemResponseDto with additional fields and constructors

--- Backend/src/main/java/com/backend/demo/Dto/Cart/CartItemResponseDto.java
+++ Backend/src/main/java/com/backend/demo/Dto/Cart/CartItemResponseDto.java
@@ -1,4 +1,31 @@
 package com.backend.demo.Dto.Cart;
 
+import com.backend.demo.Dto.Category.CategorySummaryDto;
+import com.backend.demo.Entities.Cart;
+import com.backend.demo.Entities.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class CartItemResponseDto {
+    private Long Id;
+    private Long productId;
+    private CategorySummaryDto category;
+    private BigDecimal price;
+    private String imageUrl;
+    private int quantity;
+    private Cart cart;
+    private Date added_at;
+
+    public void setCategory(Category category) {
+
+    }
 }
