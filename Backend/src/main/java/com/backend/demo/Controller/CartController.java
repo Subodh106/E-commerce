e99f824e0bbfbaecae3b0bcc7be31e9d66593e68
Subodh106 <subodhloves106@gmail.com>
2026-09-20 11:40:52 +0545

    feat(cart): update addToCart method to return CartResponseDto instead of Cart

--- Backend/src/main/java/com/backend/demo/Controller/CartController.java
+++ Backend/src/main/java/com/backend/demo/Controller/CartController.java
@@ -2,10 +2,10 @@ package com.backend.demo.Controller;
 
 import com.backend.demo.Common.ApiResponse;
 import com.backend.demo.Dto.Cart.CartItemDto;
+import com.backend.demo.Dto.Cart.CartResponseDto;
 import com.backend.demo.Entities.Cart;
 import com.backend.demo.Security.CustomUserPrincipal;
 import com.backend.demo.Service.CartService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/cart")
@@ -25,10 +23,10 @@ public class CartController {
 
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Cart>> addToCart(@RequestBody CartItemDto cartItemDto, @AuthenticationPrincipal CustomUserPrincipal customUserPrincipal){
+    public ResponseEntity<ApiResponse<CartResponseDto>> addToCart(@RequestBody CartItemDto cartItemDto, @AuthenticationPrincipal CustomUserPrincipal customUserPrincipal){
         Long userId = customUserPrincipal.getId();
-        Cart response = cartService.addToCart(cartItemDto , userId);
-        ApiResponse<Cart> cartResponse = new ApiResponse<>("Item added to cart successfully",response);
+        CartResponseDto response = cartService.addToCart(cartItemDto , userId);
+        ApiResponse<CartResponseDto> cartResponse = new ApiResponse<>("Item added to cart successfully",response);
 
     return ResponseEntity.status(201).body(cartResponse);
     }
