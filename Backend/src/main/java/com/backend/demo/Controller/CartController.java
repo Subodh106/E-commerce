7235207511b77fd319bbbac8d976961290e37c92
Subodh106 <subodhloves106@gmail.com>
2026-09-23 20:25:57 +0545

    fix(cartController): remove unused import for cleaner code

--- Backend/src/main/java/com/backend/demo/Controller/CartController.java
+++ Backend/src/main/java/com/backend/demo/Controller/CartController.java
@@ -6,7 +6,6 @@ import com.backend.demo.Dto.Cart.CartResponseDto;
 import com.backend.demo.Security.CustomUserPrincipal;
 import com.backend.demo.Service.CartService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
