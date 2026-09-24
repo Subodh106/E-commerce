eb54e85db6b2affb425d6da4f46d27313770dece
Subodh106 <subodhloves106@gmail.com>
2026-09-24 22:49:24 +0545

    feat(cartService): add getCart function to retrieve items from the cart

--- frontend/services/cartService.ts
+++ frontend/services/cartService.ts
@@ -3,3 +3,7 @@ import { api } from "@/lib/api"
 export const AddToCart = (addToCartItems:{productId:string,quantity:number})=>{
     return api.post("/cart",{addToCartItems})
 }
+
+export const getCart = ()=>{
+    return api.get("/cart");
+}
