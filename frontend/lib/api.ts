1b9d6baf9f242426e8bf20e36f8d5bfa1511d616
Subodh106 <subodhloves106@gmail.com>
2026-09-24 22:20:53 +0545

    fix(api): implement apiFetch function and define api configuration

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,8 +1,9 @@
+import axios from "axios";
 
-export async function apiFetch<T>(path : string , method:string , init?:RequestInit) {
-    
-}
-
-export const api ={
-
-}
+export const api = axios.create({
+    baseURL:process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL||"http://localhost:8080/api/v1",
+    withCredentials:true,
+    headers:{
+        "Content-Type":"application/json"
+    }
+});
