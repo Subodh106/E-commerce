0fcd535bb803e97b7b2fefa314705d9b4856d27b
Subodh106 <subodhloves106@gmail.com>
2026-09-25 22:12:31 +0545

    fix(CartResponseDto): add subtotal, shipping, and total fields to CartResponseDto class

--- Backend/src/main/java/com/backend/demo/Dto/Cart/CartResponseDto.java
+++ Backend/src/main/java/com/backend/demo/Dto/Cart/CartResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -17,6 +18,9 @@ public class CartResponseDto {
     private Long id;
     private Long user_id;
     private List<CartItemResponseDto> cartItems= new ArrayList<>();
+    private BigDecimal subtotal;
+    private BigDecimal shipping;
+    private BigDecimal total;
     private Date create_at;
     private Date updated_at;
 }
